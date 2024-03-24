69d310636d26c52e5a329d19296bc6000f152536
isabella <bellamanope@gmail.com>
2024-03-24 16:56:00 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_1302223062/Program.cs
+++ tpmodul6_1302223062/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        this.id = GenerateRandomId();
+        this.title = title;
+        this.playCount = 0;
+    }
+
+    private int GenerateRandomId()
+    {
+        Random random = new Random();
+        return random.Next(10000, 99999);
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        playCount += count;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine($"Video Details:\nID: {id}\nTitle: {title}\nPlay Count: {playCount}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string namaPraktikan = "[Francisca_PRAKTIKAN]";
+        string judulVideo = $"Tutorial Design By Contract - {namaPraktikan}";
+
+        SayaTubeVideo video = new SayaTubeVideo(judulVideo);
+        video.IncreasePlayCount(5); 
+        video.PrintVideoDetails();
+    }
+}
